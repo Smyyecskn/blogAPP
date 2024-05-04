786420bcd697cf17da7801b659f97036283a7fbf
smyye <smyyeoztrk43@gmail.com>
2024-05-04 11:36:20 +0300

    ok vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,7 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello Blog App!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-center mt-5">
+      Hello Blog App!
+    </h1>
+  );
 }
